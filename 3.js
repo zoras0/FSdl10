9c5f7cd2a084e797e1464597da4b6cfa0ce236ec
zoras0 <168116990+zoras0@users.noreply.github.com>
2025-02-03 07:15:00 +0000

    gfhfg

---
+++ 3.js
@@ -0,0 +1,5 @@
+const button = document.getElementById("myButton");
+button.addEventListener("click", function(event) {
+    // Prevent the default action of the anchor tag
+    event.preventDefault();
+});
