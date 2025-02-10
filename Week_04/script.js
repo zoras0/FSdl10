380dd0f2878ed5843f262bf917b71b9f14f821a0
zoras0 <168116990+zoras0@users.noreply.github.com>
2025-02-10 07:01:33 +0000

    d

--- Week_04/script.js
+++ Week_04/script.js
@@ -1,9 +1,6 @@
-// script.js
 document.getElementById('agebutton').addEventListener('click', function() {
     const inputNumber = document.getElementById('numberInput').value;
-    const multiplier = 13; // 
+    const multiplier = 4; // 
     const result = inputNumber * multiplier;
-
-    // Display the result
     document.getElementById('result').innerText = `Your Age : ${result}`;
 });
